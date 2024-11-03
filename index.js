868a6048297ad3ad3a0e3e4c3e7f92780d483e5e
Tsurematsu <daniel120802@gmail.com>
2024-11-02 20:03:14 -0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 // Importa Puppeteer utilizando sintaxis ES6
 import puppeteer from 'puppeteer';
 import path from 'path';
-import { readFile, writeFile } from 'fs/promises';
+import { readFile, writeFile, readdir } from 'fs/promises';
 import { join } from 'path';
-import readline from 'readline';
+import inquirer from 'inquirer';
 
-// Crear una interfaz para leer desde la consola
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 // Directorio para el perfil de usuario
 const userDataDir = path.join(process.cwd(), 'user_data');
 const pathToExtension = path.join(process.cwd(), './Adblock-Plus-bloqueador-de-anuncios-gratis-Chrome-Web-Store');
@@ -19,16 +14,120 @@ const args = [
     `--load-extension=${pathToExtension}`,
     '--no-sandbox',
     '--disable-setuid-sandbox',
-    // '--window-position=2000,0'
 ];
-console.log("-------->", args);
-
-
 let browser;
 let pageMusic;
 let page;
-main()
+let configInit;
+let urlConfigFile="./config_default.json";
+let onReadyToStep = ()=>{};
+async function showMenu() {
+    console.clear();
+    const filesConfig = await getConfigJsonFiles();
+    const choices = [...filesConfig, "make new config", "remove config", "exit"];
+    const answer = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'config',
+            message: 'Seleccione un archivo de configuración:',
+            choices: choices
+        }
+    ]);
+    if (answer.config === "make new config") {
+        console.clear();
+        const responses = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'nameConfig',
+                message: 'Config name:'
+            },
+            {
+                type: 'input',
+                name: 'channelURL',
+                message: 'Channel URL:'
+            },
+            {
+                type: 'input',
+                name: 'channelName',
+                message: 'Channel name:'
+            },
+            {
+                type: 'input',
+                name: 'botName',
+                message: 'Bot name:'
+            },
+            {
+                type: 'input',
+                name: 'tagBot',
+                message: 'Bot tag:'
+            },
+            {
+                type: 'input',
+                name: 'selectorScroll',
+                message: 'Selector scroll (optional):',
+                default: '.scroller_c43953.thin_eed6a8.scrollerBase_eed6a8.fade_eed6a8.customTheme_eed6a8'
+            },
+        ]);
+
+        const config = {
+            channelURL: responses.channelURL,
+            channelName: responses.channelName,
+            botName: responses.botName,
+            selectorScroll: responses.selectorScroll,
+            tagBot: responses.tagBot
+        };
+        await writeJson(`config_${responses.nameConfig}.json`, config);
+        urlConfigFile = `config_${responses.nameConfig}.json`;
+        showMenu();
+    }else if(answer.config === "remove config"){
+        const choices = [...filesConfig, "exit"];
+        const answer = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'config',
+                message: 'Seleccione un archivo de configuración:',
+                choices: choices
+            }
+        ]);
+        if (answer.config === "exit") {
+            console.clear();
+            showMenu();
+        }else{
+            const filePath = join(process.cwd(), answer.config);
+            console.log(`Archivo ${answer.config} eliminado exitosamente.`, filePath);
+            
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error(`Error al eliminar el archivo: ${err.message}`);
+                    return;
+                }
+                console.log('Archivo eliminado exitosamente.');
+            });
+            
+        }
+
+    }else if (answer.config === "exit") {
+        console.clear();
+        return;
+    } else {
+        urlConfigFile = answer.config;
+        main();
+    }
+    
+}
+
+showMenu();
 async function main() {
+    configInit = await readJson(urlConfigFile);
+    console.clear();
+    console.log("> configInit: ", configInit);
+    await new Promise(resolve => setTimeout(resolve, 800));
+    question();
+    const {channelURL, channelName, selectorScroll, botName, tagBot} = configInit;
+    
+    console.clear();
+    console.log("> starting: ", channelURL);
+    
     // Lanza un navegador sin cabeza (headless)
     browser = await puppeteer.launch({
         headless: false, // Cambia a false para mostrar la ventana
@@ -41,6 +140,10 @@ async function main() {
     pageMusic = await browser.newPage();
     await new Promise(resolve => setTimeout(resolve, 1000));
     page = await browser.newPage();
+
+    console.clear();
+    console.log("> loading page discord and music\n>");
+    
     const gotoMusic = async (url) => {
         pageMusic.bringToFront();
         await pageMusic.goto(url, {
@@ -50,6 +153,7 @@ async function main() {
         await new Promise(resolve => setTimeout(resolve, 2000));
         page.bringToFront();
     };
+
     const controlsMusic = async (action, value = null) => {
         const controls = {
             pause: async () => {
@@ -83,16 +187,21 @@ async function main() {
         }
         await controls[action]?.(value);
     }
+
     await pageMusic.goto('about:blank');
     page.on('console', msg => {
         const intoText = msg.text();
-        if (intoText.includes('[devMSG]')) {
-            console.log(intoText);
+        const tag = '[devMSG]';
+        const tagClear = '[clear]';
+        if (intoText.includes(tag)) {
+            console.log(intoText.split(tag)[1]);
+        }else if (intoText === tagClear) {
+            console.clear();
         }
     });
 
     // Navega a una URL específica
-    await page.goto('https://discord.com/channels/768278151435386900/845314420494434355');
+    await page.goto(channelURL);
     await page.exposeFunction('readUsers', async () => {
         return await readUsers();
     });
@@ -105,54 +214,127 @@ async function main() {
     await page.exposeFunction('controlsMusic', async (into, value) => {
         return await controlsMusic(into, value);
     });
+    await page.exposeFunction('pressChat', async () => {
+        await pressChat();
+        return true;
+    });
+    await page.exposeFunction('nextStep', async () => {
+        onReadyToStep();
+        return true;
+    });
 
-    const selector = '.scroller_c43953.thin_eed6a8.scrollerBase_eed6a8.fade_eed6a8.customTheme_eed6a8';
+    console.clear();
+    console.log("> Page loaded\n>");
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    const selector = selectorScroll;
     await page.waitForSelector(selector);
-    console.log("--------> selector: ", selector);
+    console.clear();
+    console.log(">Starting...\n>");
     
     // Establecer el scroll hacia el fondo del div
-    await page.evaluate((selector) => {
-        console.log("--------> script1");
+    await page.evaluate((selector, channelName, botName) => {
+        function log(...args) {console.log("[devMSG]", ...args);}
+        function clear() {console.log("[clear]");}
+        clear();
+        // log( ">Loading script for login channel\n>");
         script1();
         async function script1() {
-            const element = document.querySelector(selector);
-            if (element) {
-                element.scrollTop = element.scrollHeight;
+            listUsers();
+            async function listUsers(){
+                // const channelName = "Tsure's Channel";
+                // const selector = ".scroller_c43953.thin_eed6a8.scrollerBase_eed6a8.fade_eed6a8.customTheme_eed6a8";
                 await new Promise(resolve => setTimeout(resolve, 1000));
-                const elements = document.querySelectorAll("li");
-                for (const element of elements) {
-                    const dndName = element.getAttribute('data-dnd-name');
-                    if (dndName === "Tsure's Channel") {
-                        const onDiv = element.querySelectorAll("div")[0];
-                        const aButton = onDiv.querySelectorAll("a")[0];
-                        aButton.click();
-                        await new Promise(resolve => setTimeout(resolve, 1000));
-                        const parentChannel = document.querySelectorAll(".inner_adcaac")[0].querySelectorAll("div")[0];
-                        const aChannel = parentChannel.querySelectorAll("a")[0];
-                        aChannel.click();
+                try {
+                    console.log("Loading function list users");
+                    const lateralBar = document.querySelector(selector);
+                    if (lateralBar === null) {listUsers(); return};
+                    lateralBar.scrollTop = lateralBar.scrollHeight;
+                    await new Promise(resolve => setTimeout(resolve, 500));
+                    const listChannels = lateralBar.querySelectorAll("li");
+                    if (listChannels.length === 0) {listUsers(); return};
+                    let channelUser = [];
+                    for (const channel of listChannels) {
+                        const dndName = channel.getAttribute('data-dnd-name');
+                        if (dndName === channelName) {channelUser.push(channel);}
                     }
+                    if (channelUser.length === 0) {listUsers(); return};
+                    channelUser = channelUser[0];
+                    console.log("Channel found, ", channelUser);  
+                    selectChannel(channelUser);
+                    return;
+                } catch (error) {
+                    console.log("Error: ", error.message);
+                    listUsers()
+                }
+            }
+
+            async function selectChannel(elementChanelUser){
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                try {
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                    const parentDivChannel = elementChanelUser.querySelectorAll("div")[0];
+                    if (parentDivChannel === null) {selectChannel(elementChanelUser); return};
+                    const intoChannelButton = parentDivChannel.querySelectorAll("a")[0];
+                    if (intoChannelButton === null) {selectChannel(elementChanelUser); return};
+                    intoChannelButton.click();
+                    entryChannel();
+                } catch (error) {
+                    selectChannel(elementChanelUser);
+                }
+            }
+
+            async function entryChannel(){
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                try {
+                    const identifierUserTag = botName;
+                    const identifierElement = document.querySelectorAll("div");
+                    let onElementUser = null;
+                    for (const idUser of identifierElement) {
+                        const elementText = idUser.innerHTML;
+                        if (elementText === identifierUserTag) {
+                            onElementUser = idUser;
+                        }
+                    }
+                    if (onElementUser === null) {entryChannel();return;}
+                    const topLevel = onElementUser.parentNode.parentNode.parentNode.parentNode.parentNode;
+                    const onSelectActionDiv = topLevel.querySelectorAll("div")[0].querySelectorAll("div")[0].querySelectorAll("div")[0];
+                    const selectorChannel = onSelectActionDiv.querySelectorAll("a")[0];
+                    selectorChannel.click();
+                    await new Promise(resolve => setTimeout(resolve, 500));
+                    await window.pressChat();
+                    await new Promise(resolve => setTimeout(resolve, 500));
+                    await window.nextStep();
+                } catch (error) {
+                    entryChannel();
                 }
             }
         }
-    }, selector);
+    }, selector, channelName, botName);
 
-    await new Promise(resolve => setTimeout(resolve, 7000));
+    console.clear();
+    console.log("--------> waiting for page...\n>");
+    await new Promise(resolve => {onReadyToStep = resolve;});
     
     // Simular la combinación de teclas Ctrl + U
-    await page.keyboard.down('Control');  // Presionar la tecla Control
-    await page.keyboard.press('KeyU');    // Presionar la tecla U
-    await page.keyboard.up('Control');    // Soltar la tecla Control
-
-    console.log("--------> loading");
+    async function pressChat(){
+        await page.keyboard.down('Control');  // Presionar la tecla Control
+        await page.keyboard.press('KeyU');    // Presionar la tecla U
+        await page.keyboard.up('Control');    // Soltar la tecla Control
+        return true;
+    }
     
-    await page.evaluate(() => {
+    console.clear();
+    console.log("--------> loading chat\n>");
+    
+    await page.evaluate((tagBotIn) => {
         main();
         async function main() {
             let onCommand = "";
-            await new Promise(resolve => setTimeout(resolve, 5000));
+            await new Promise(resolve => setTimeout(resolve, 1000));
             onChat();
             async function onChat() {
-                await new Promise(resolve => setTimeout(resolve, 5000));
+                await new Promise(resolve => setTimeout(resolve, 1000));
                 let onElement = false;
                 for (element of document.querySelectorAll('div')) {
                     const clases = element.classList;
@@ -160,7 +342,6 @@ async function main() {
                     if (clasesString.includes("chatLayerWrapper_")) {
                         onElement = true;
                     }
-
                 }
                 if (!onElement) { onChat(); return; }
                 listenChat();
@@ -191,7 +372,7 @@ async function main() {
                 onElement.shift();
                 await window.saveUsersToJson(JSON.stringify(users));
                 const ultimoComentario = onElement.pop();
-                const tagBot = "-bot "
+                const tagBot = `${tagBotIn} `;
                 const usersAdmin = await window.readUsers();
                 const conditions = async (comentario) => {
                     try {
@@ -211,6 +392,11 @@ async function main() {
                 await conditions(ultimoComentario);
                 listenChat();
             }
+
+            async function filter() {
+                
+            }
+
             async function actionCommand(command) {
                 console.log(command);
                 const commands = {
@@ -239,8 +425,26 @@ async function main() {
                 await commands[commandInto[0]]?.(commandInto.length > 1 ? commandInto[1] : null);
             }
         }
-    });
+    }, tagBot);
 
+}
+
+async function readJson(fileName) {
+    try {
+        const filePath = join(process.cwd(), fileName);
+        const jsonData = await readFile(filePath, 'utf8');
+        const data = JSON.parse(jsonData);
+        return data;
+    } catch (error) {
+        console.error(`Error reading ${fileName} file:`, error.message);
+        throw error;
+    }
+}
+
+async function writeJson(fileName, data) {
+    const filePath = join(process.cwd(), fileName);
+    await writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
+    return true;
 }
 
 async function readUsers() {
@@ -259,9 +463,13 @@ async function readUsers() {
     }
 };
 
-
-async function saveUsersToJson(newUsers) {
-    const filePath = join(process.cwd(), 'logUsers.json');
+let saveUsersToJsonCache = "";
+async function saveUsersToJson(newUsers, url="logUsers.json") {
+    if (saveUsersToJsonCache !== newUsers && url==="logUsers.json") {
+        saveUsersToJsonCache = newUsers;
+        console.log("\n\n--> users on chat: ", JSON.parse(newUsers));
+    }
+    const filePath = join(process.cwd(), url);
     await writeFile(
         filePath,
         newUsers,
@@ -270,11 +478,92 @@ async function saveUsersToJson(newUsers) {
     return true;
 };
 
-rl.question('enter to exit', async (respuesta) => {
-    if (!page.isClosed()) { await page.close(); }
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    if (!pageMusic.isClosed()) { await pageMusic.close(); }
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    await browser.close();
-    rl.close();
-});
+async function question() {
+    const answer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'response',
+            message: '>'
+        }
+    ]);
+
+    const respuesta = answer.response;
+    if (respuesta === "") {
+        console.clear();
+        console.log("--------> closing app");
+        try {
+            if (!page.isClosed()) { await page.close(); }
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            if (!pageMusic.isClosed()) { await pageMusic.close(); }
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            if (browser) { await browser.close(); }
+            console.log("--------> browser closed");
+            await new Promise(resolve => setTimeout(resolve, 500));
+            console.clear();
+        } catch (error) {}
+        console.log("--------> app closed");
+        
+        return;
+    } else if (respuesta.includes("add")) {
+        const newUser = respuesta.split(" ")[1];
+        let onUsers = await readUsers();
+        let obj = onUsers.reduce((acc, key) => {
+            acc[key] = "";
+            return acc;
+        }, {});
+        obj[newUser] = "";
+        onUsers = Object.keys(obj);
+        console.clear();
+        console.log(`Okay adding ${newUser}------->`, onUsers);
+        await saveUsersToJson(JSON.stringify(onUsers), "users.json");
+        await new Promise(resolve => setTimeout(resolve, 800));
+        console.clear();
+        saveUsersToJsonCache = "";
+        question();
+        return;
+    } else if (respuesta.includes("remove")) {
+        const newUser = respuesta.split(" ")[1];
+        let onUsers = await readUsers();
+        let obj = onUsers.reduce((acc, key) => {
+            acc[key] = "";
+            return acc;
+        }, {});
+        delete obj[newUser];
+        onUsers = Object.keys(obj);
+        console.clear();
+        console.log(`Okay removing ${newUser}------->`, onUsers);
+        await saveUsersToJson(JSON.stringify(onUsers), "users.json");
+        await new Promise(resolve => setTimeout(resolve, 800));
+        console.clear();
+        saveUsersToJsonCache = "";
+        question();
+        return;
+    } else if (respuesta.includes("list")) {
+        let onUsers = await readUsers();
+        console.log(`Okay listing users------->`, onUsers);
+        await new Promise(resolve => setTimeout(resolve, 20000));
+        console.clear();
+        saveUsersToJsonCache = "";
+        question();
+        return;
+    }
+    console.clear();
+    saveUsersToJsonCache = "";
+    question();
+}
+
+async function getConfigJsonFiles() {
+    try {
+        const directoryPath = process.cwd();
+        const files = await readdir(directoryPath);
+        const configJsonFiles = files.filter(file => file.endsWith('.json') && file.includes('config'));
+        return configJsonFiles;
+    } catch (error) {
+        console.error('Error reading directory:', error.message);
+        throw error;
+    }
+}
+
+
+// "Tsure",
+// sαdkσdєv
