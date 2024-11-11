f6cea0bcc945f5868333de7811c81bf9ff6c843d
Tsurematsu <daniel120802@gmail.com>
2024-11-11 15:06:19 -0500

    feat(discord): optimize server selection logic; replace find with for loop and add null check

--- src/discord/Server/Select.ts
+++ src/discord/Server/Select.ts
@@ -7,10 +7,18 @@ class Select {
 	}
 	async main(nameSever) {
 		this.listServers = await listServer(this.page)
-		const { data_list_item_id } = this.listServers.find((server) =>
-			server.name.includes(nameSever),
-		)
+		let data_list_item_id = null
+		for (const element of this.listServers) {
+			if (element.name.includes(nameSever)) {
+				data_list_item_id = element.data_list_item_id
+				break
+			}
+		}
+		if (data_list_item_id === null) {
+			return false
+		}
 		await this.page.click(`div[data-list-item-id="${data_list_item_id}"]`)
+		return true
 	}
 }
 export default Select
