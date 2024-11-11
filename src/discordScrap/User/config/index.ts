bcf00f7e8788d19bbdba5285dbcefd2d8492b29a
Tsurematsu <daniel120802@gmail.com>
2024-11-11 16:09:33 -0500

    feat(discordScrap): refactor config methods; simplify open/close logic and enhance input handling

--- src/discordScrap/User/config/index.ts
+++ src/discordScrap/User/config/index.ts
@@ -5,24 +5,13 @@ import openConfig from './modules/openConfig'
 const errorMSG =
 	'Config not open, please use open() method. [await Config.open()]'
 class Config {
-	private flagOpen = false
-	private onOpen(callback) {
-		if (!this.flagOpen) throw new Error(errorMSG)
-		return callback
-	}
 	page: Page
 	input: input['main']
 	constructor(page) {
 		this.page = page
-		this.input = this.onOpen(new input(this.page).main)
+		this.input = new input(page).main
 	}
-	async open() {
-		await openConfig(this.page)
-		this.flagOpen = true
-	}
-	async close() {
-		await closeConfig(this.page)
-		this.flagOpen = false
-	}
+	open = () => openConfig(this.page)
+	close = () => closeConfig(this.page)
 }
 export default Config
