19a111b6ea1159837e177121af59acdd08272e1b
Tsurematsu <daniel120802@gmail.com>
2024-11-11 23:32:15 -0500

    feat(sensibility): implement scrolling functionality for slider interaction; add scrollToElement module

--- src/discordScrap/User/config/VoiceAndVideo/sensibility/index.ts
+++ src/discordScrap/User/config/VoiceAndVideo/sensibility/index.ts
@@ -1,6 +1,7 @@
 import type { Page } from 'puppeteer'
 import selectOption from '../../modules/selectOption'
 import getProperty from './modules/getProperty'
+import scrollToElement from './modules/scrollToElement'
 export default class Sensibility {
 	page: Page
 	constructor(page: Page) {
@@ -8,20 +9,29 @@ export default class Sensibility {
 	}
 	async set(value) {
 		await selectOption(this.page, 'Voz y vídeo')
-		await new Promise((resolve) => setTimeout(resolve, 5000))
+		await this.page.waitForSelector('div[role="tabpanel"]')
 		const sliderClass = await getProperty(this.page)
 		const classA = `.${sliderClass.replace(' ', '.')}`
-		const slider = await this.page.$(classA)
-		if (slider) {
-			// Obtener las coordenadas del slider (posición y tamaño)
-			const boundingBox = await slider.boundingBox()
-			// Definir la posición en el slider donde hacer clic (por ejemplo, en el centro)
-			const xPos = boundingBox.x + (value + 1)
-			const yPos = boundingBox.y + boundingBox.height / 2
-			// Simular un clic en esa posición
-			await this.page.mouse.click(xPos, yPos)
-			return true
+		await scrollToElement(this.page, classA)
+		const scrolling = async () => {
+			if (await this.page.$(classA)) {
+				const slider = await this.page.$(classA)
+				// Obtener las coordenadas del slider (posición y tamaño)
+				const boundingBox = await slider.boundingBox()
+				// Definir la posición en el slider donde hacer clic (por ejemplo, en el centro)
+				const segments = boundingBox.width / 100
+				const porcentaje = segments * value
+				const valueScale = porcentaje + (value <= 0 ? 1 : 0)
+				const maxValueScale =
+					valueScale >= boundingBox.width ? boundingBox.width - 1 : valueScale
+				console.log('maxValueScale:', maxValueScale)
+				const xPos = boundingBox.x + maxValueScale
+				const yPos = boundingBox.y + boundingBox.height / 2 + 1
+				await this.page.mouse.click(xPos, yPos)
+			}
 		}
-		return false
+		await scrolling()
+		// console.log('No se ha podido cambiar la sensibilidad de voz')
+		// return false
 	}
 }
---
+++ src/discordScrap/User/config/VoiceAndVideo/sensibility/modules/scrollToElement.ts
@@ -0,0 +1,5 @@
+import type { Page } from 'puppeteer'
+
+export default async function scrollToElement(page: Page, classA) {
+	console.log('scrollToElement')
+}
